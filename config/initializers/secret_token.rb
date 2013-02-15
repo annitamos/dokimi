6c63889e625ad705094b3bfbe480851c73447b53
amorou <annitamos@hotmail.com>
2013-02-15 15:24:31 +0200

    dff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dokimi::Application.config.secret_token = 'ee299e814901e73c434051f9256a865c1a17b4fb88597aae57a351e072643c05728dd71f6cc016e54923c9f51469366647d5e0939b70343e277d05601b98b04a'
